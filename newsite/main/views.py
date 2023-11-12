d1bb9f42d57609b911d722c9352b5bb0620e1cc8
«Oleg» <Pan159dom159@yandex.ru>
2023-11-12 16:03:48 +0300

    дОБАВИЛ ВТОРУЮ СТРАНИЧКУ О НАС

--- newsite/main/views.py
+++ newsite/main/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 
 
-def index(request):   # главная страница
-    return render(request, '')
+def index(request):   # Главная страница
+    return render(request, 'main/index.html')
+
+
+def abaut(request):    # О нас
+    return render(request, 'main/abaut.html')
 
 
